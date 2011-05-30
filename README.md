455241ef56c8a6fea7e2992ed9fb2a7c48817ea6
Andy <andy.farrell@phgroup.com>
2011-05-30 11:59:02 +0100

    Basic event listeners and stuff in place.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+##Note: QuickSlide doesn't actually do anything useful yet :-)
+
 #QuickSlide
 
 QuickSlide is a simple JavaScript popup image viewer, ideal for lightweight
@@ -6,9 +8,17 @@ gallery pages.
 Adding it to a page is simple:
 
 1. Include the `quickslide.js` file at the bottom of your page:
+
         <script src="quickslide.js"></script>
 
 2. Mark your thumbnail image links with `rel="quickslide"`:
+
         <a href="myphoto_big.jpg" rel="quickslide">
-		    <img src="myphoto_thumb.jpg">
-		</a>
+            <img src="myphoto_thumb.jpg">
+        </a>
+
+3. Add the `quickslide` class to a parent element:
+
+        <div class="gallery-container quickslide">
+          <!-- thumbnail image links go here -->
+        </div>
--- quickslide.js
+++ quickslide.js
@@ -1,3 +1,65 @@
+/*globals window */
 (function () {
-	var q;
+	var normalizeEvent, addListener, setupGalleryLinks, createPopup,
+		getLinkNodes;
+
+	// Couple of convenience functions for dealing with events.
+	normalizeEvent = function (e) {
+		if (!e.stopPropagation) {
+			e.stopPropagation = function () {
+				this.cancelBubble = true;
+			};
+
+			e.preventDefault = function () {
+				this.returnValue = false;
+			};
+		}
+
+		if (e.srcElement && !e.target) {
+			e.target = e.srcElement;
+		}
+
+		return e;
+	};
+
+	addListener = function (node, type, handler) {
+		var wrapHandler = function (e) {
+			// Make 'this' inside the handler refer to the 'node' parameter.
+			handler.apply(node, [normalizeEvent(e || window.event)]);
+		};
+
+		if (typeof node.attachEvent === "function") {
+			node.attachEvent("on" + type, wrapHandler);
+		} else {
+			node.addEventListener(type, wrapHandler);
+		}
+
+		return { node: node, type: type, handler: wrapHandler };
+	};
+
+	setupGalleryLinks = function () {
+		var galleryLinks, i, len;
+
+		galleryLinks = document.querySelectorAll(".quickslide a");
+
+		// Assign a separate click handler for each gallery, so that the
+		// handler will loop through only the links in its gallery.
+		for (i = 0, len = galleryLinks.length; i < len; i += 1) {
+			addListener(galleryLinks[i], "click", function (e) {
+				e.preventDefault();
+				createPopup(this);
+			});
+		}
+	};
+
+	createPopup = function (fromNode) {
+		var links, i, len;
+
+		links = fromNode.parentNode.querySelectorAll("a");
+
+	};
+
+	addListener(window, "load", function () {
+		setupGalleryLinks();
+	});
 }());
